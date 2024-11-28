63195c950b7f703ae658ac8cf24c04e3281dd059
Zouhir El Amraoui <zouhir.mpi.isitd@gmail.com>
2024-11-28 21:10:38 +0100

    First Configuration Setup for cloud project

---
+++ src/test/java/cloud_project/CloudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
